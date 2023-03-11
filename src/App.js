a985cd7e6f0554a8613142ed48fcb589f5500c7b
jakewhy <jwyboyles@gmail.com>
2023-03-11 17:59:37 -0600

    set up API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from "react";
 
 function App() {
+
+  const [word, setWord] = useState("")
+
+
+  function handleChange(event){
+    setWord(event.target.value)
+  }
+
+  function sendApi() {
+    console.log("click")
+    
+    const url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=e310c050-e024-4fb8-b013-fc82a6786891`
+    let req = new Request(url);
+  
+    fetch(req)
+      .then(function(response) {
+          return (response.json());
+      }).then(function(response) {
+        console.log(response)
+      }).catch(function(error) {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <label>Choose a word</label>
+        <input            
+           type="text"
+            placeholder='Word'
+            name="word"
+            onChange={handleChange}
+            value={word}
+        />
+      <button onClick={sendApi}>Send Api</button>
+      <p>{word}</p>
     </div>
   );
 }
